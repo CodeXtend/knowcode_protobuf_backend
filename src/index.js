aeec227c16002b6385f47b51e8ea543622722080
Aditya Basude <adityabasude@ternaengg.ac.in>
2025-01-25 01:15:38 +0000

    updated cors

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./db/utils/dbConnect.js";
 import morgan from "morgan";
-import userRoutes from './routes/userRoute.js';
-import wasteRoutes from './routes/wasteRoute.js';
+import userRoutes from "./routes/userRoute.js";
+import wasteRoutes from "./routes/wasteRoute.js";
 
 dotenv.config();
 
@@ -14,20 +14,20 @@ const port = process.env.PORT || 3000;
 // Initialize MongoDB connection
 connectDB();
 
-app.use(cors());
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // Add routes
-app.use('/api/v1/users', userRoutes);
-app.use('/api/v1/waste', wasteRoutes);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/waste", wasteRoutes);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({
-    status: 'error',
-    message: 'Something went wrong!'
+    status: "error",
+    message: "Something went wrong!",
   });
 });
 
