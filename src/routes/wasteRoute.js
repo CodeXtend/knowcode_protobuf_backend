2d31d9c797729412598028c7624a7df971ee6941
Aditya Basude <adityabasude@ternaengg.ac.in>
2025-01-25 16:26:30 +0000

    fx: fx the dashboard ssue

--- src/routes/wasteRoute.js
+++ src/routes/wasteRoute.js
@@ -3,13 +3,15 @@ import * as wasteController from "../controllers/wasteController.js";
 
 const router = express.Router();
 
-// Public routes
-router.get('/search', wasteController.searchWaste);
-router.get('/:id', wasteController.getWaste);
-router.post('/add', wasteController.createWasteEntry);
-
-// Analytics routes
+// Analytics and stats routes (place these BEFORE the :id route)
 router.get('/stats', wasteController.getWasteStats);
 router.get('/analytics/monthly', wasteController.getMonthlyAnalytics);
+
+// Public routes
+router.get('/search', wasteController.searchWaste);
+router.post('/add', wasteController.createWasteEntry);
+
+// Parameter route should be last
+router.get('/:id', wasteController.getWaste);
 
 export default router;
